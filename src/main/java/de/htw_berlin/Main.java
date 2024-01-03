3c3733535964fee83840cc2d1bc0ceb276ccd6f7
Lezhor <dani.serdyuk@gmail.com>
2024-01-03 14:49:11 +0100

    Initial Commit

---
+++ src/main/java/de/htw_berlin/Main.java
@@ -0,0 +1,16 @@
+package de.htw_berlin;
+
+/**
+ * Main-class. This is where to launch the program
+ */
+public class Main {
+
+    /**
+     * Main-method
+     * @param args empty
+     */
+    public static void main(String[] args) {
+        // TODO: 03.01.2024 Start Server
+    }
+
+}
---
+++ src/test/java/de/htw_berlin/MainTest.java
@@ -0,0 +1,19 @@
+package de.htw_berlin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Example test class
+ */
+public class MainTest {
+
+    /**
+     * Example test
+     */
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
