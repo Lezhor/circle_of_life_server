32602bef069f97661717c3ff2c7bdfaeacdbcca2
Lezhor <dani.serdyuk@gmail.com>
2024-01-04 17:05:19 +0100

    Engine: made SyncEngineImpl singleton

--- src/main/java/de/htw_berlin/application/App.java
+++ src/main/java/de/htw_berlin/application/App.java
@@ -2,6 +2,8 @@ package de.htw_berlin.application;
 
 import de.htw_berlin.engines.AuthenticationEngine;
 import de.htw_berlin.engines.AuthenticationEngineImpl;
+import de.htw_berlin.engines.SyncEngine;
+import de.htw_berlin.engines.SyncEngineImpl;
 
 /**
  * offers getters for all singleton classes
@@ -10,6 +12,10 @@ public final class App {
 
     public static AuthenticationEngine getAuthentication() {
         return AuthenticationEngineImpl.getInstance();
+    }
+
+    public static SyncEngine getSyncEngine() {
+        return SyncEngineImpl.getInstance();
     }
 
 }
--- src/main/java/de/htw_berlin/engines/SyncEngineImpl.java
+++ src/main/java/de/htw_berlin/engines/SyncEngineImpl.java
@@ -10,6 +10,26 @@ import java.util.List;
  * Implements the {@link SyncEngine}
  */
 public class SyncEngineImpl implements SyncEngine {
+
+    private static volatile SyncEngineImpl instance;
+
+    public static SyncEngineImpl getInstance() {
+        if (instance == null) {
+            synchronized (SyncEngineImpl.class) {
+                if (instance == null) {
+                    instance = new SyncEngineImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * Private constructor for singleton
+     */
+    private SyncEngineImpl() {
+    }
+
     @Override
     public List<DBLog<?>> sync(LocalDateTime lastSync, DBLog<?>... logs) {
         // TODO: 04.01.2024 implement syncing with db
