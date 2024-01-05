ad7b546b1fe53d48851cb1577d3464d77c105257
Lezhor <dani.serdyuk@gmail.com>
2024-01-05 23:11:46 +0100

    Main: Removed random testing from main method

--- src/main/java/de/htw_berlin/application/Main.java
+++ src/main/java/de/htw_berlin/application/Main.java
@@ -1,11 +1,8 @@
 package de.htw_berlin.application;
 
 import de.htw_berlin.communication.Server;
-import de.htw_berlin.database.models.User;
 import de.htw_berlin.logging.Log;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 /**
  * Main-class. This is where to launch the program
@@ -23,18 +20,6 @@ public class Main {
         Server server = new Server();
         server.start();
         Runtime.getRuntime().addShutdownHook(new Thread(server::shutDown));
-
-        User user = new User(UUID.randomUUID(), "alice_connor", "kdjabskk", LocalDateTime.now());
-
-        Log.d(TAG, "Exists: " + App.getDatabaseController().exists(user));
-        App.getDatabaseController().insert(user);
-        Log.d(TAG, "Exists: " + App.getDatabaseController().exists(user));
-
-        User user2 = App.getDatabaseController().getById(user.getId(), User.class);
-
-        Log.d(TAG, "Equals: " + user.equalsAllParams(user2));
-
-        App.getDatabaseController().delete(user);
 
     }
 
